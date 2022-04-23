474f76cab3666136c6421dc2f04ac300c8fac9b4
Catermelon <66882633+darkdarcool@users.noreply.github.com>
2022-04-23 08:04:46 -0700

    Added docs(just the regions txt currently), and added more data to the agent class

--- src/classes/Agent.ts
+++ src/classes/Agent.ts
@@ -2,12 +2,16 @@ export type AgentData = {
   name: string,
   localizedNames: {
     [key: string]: string
-  }
+  },
+  id: string,
+  assetName: string,
 }
 
 export class Agent {
   private agentData: AgentData;
   private curRegionName: string;
+  private id: string;
+  private assetName: string;
   private localizedNames;
 
   constructor(agentData: AgentData) {
@@ -22,11 +26,20 @@ export class Agent {
 
     this.curRegionName = data.name;
     this.localizedNames = data.localizedNames;
+    this.id = data.id;
+    this.assetName = data.assetName;
   }
-  getCurRegionName() {
+  getName() {
     return this.curRegionName;
   }
   getLocalizedName(regionName: string) {
+    console.log(this.localizedNames);
     return this.localizedNames[regionName];
+  }
+  getID() {
+    return this.id;
+  }
+  getAssetName() {
+    return this.assetName;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import Client from "./Client";
 let client = new Client("na", "RGAPI-554966d9-1f8a-4100-a278-e56d35fb834e");
 
 client.getAgent("Omen").then(agent => {
-  console.log(agent.getLocalizedName("zh-CN"));
+  console.log(agent.getID());
 });
